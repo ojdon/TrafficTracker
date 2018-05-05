1af839bc1bb11e85eed35c28376bdd84963ceec5
Ollie Reardon <ojreardon@gmail.com>
2018-05-05 15:01:15 +0100

    New version.

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -3,8 +3,8 @@
 		<nav>
 			<b-navbar type="dark" variant="primary" fixed="top">
 				<b-container>
-					<b-navbar-nav>
-						<b-nav-item to="/" left>Back</b-nav-item>
+					<b-navbar-nav v-if="hideBack === false">
+						<b-nav-item v-on:click="goBack" left>Back</b-nav-item>
 					</b-navbar-nav>
 					<b-navbar-brand to="/" class="mx-auto">TrafficTracker</b-navbar-brand>
 					<b-navbar-toggle target="nav_collapse" right></b-navbar-toggle>
@@ -25,10 +25,35 @@
 </template>  
 <script>
 export default {
+
 	name: 'appHeader',
+
 	data () {
 		return {
-			msg: 'Welcome to Your Vue.js App'
+			msg: 'Welcome to Your Vue.js App',
+			hideBack: false
+		}
+
+
+	},
+	created() {
+		this.checkBack();
+	},
+	methods: {
+		checkBack: function() {
+			if(this.$router.history.current.name === 'home') {
+				this.hideBack = true
+			} else {
+				this.hideBack = false
+			}
+		},
+		goBack: function() {
+			this.$router.go(-1);
+		}
+	},
+	watch:{
+		$route (to, from){
+			this.checkBack();
 		}
 	}
 }
