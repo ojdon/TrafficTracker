f3cfc388080d1d9064343761262d1e85ae3f2d8c
Ollie Reardon <ojreardon@gmail.com>
2018-05-08 08:52:56 +0100

    Added icons to the home buttons

--- src/components/partials/Header.vue
+++ src/components/partials/Header.vue
@@ -7,15 +7,6 @@
 						<b-nav-item v-on:click="goBack" left>Back</b-nav-item>
 					</b-navbar-nav>
 					<b-navbar-brand to="/" class="mx-auto">TrafficTracker</b-navbar-brand>
-					<b-navbar-toggle target="nav_collapse" right></b-navbar-toggle>
-					<b-collapse is-nav id="nav_collapse">
-
-						<b-navbar-nav class="mx-auto text-center">
-							<b-nav-item to="accidents">Accidents</b-nav-item>
-							<b-nav-item to="roadworks">Roadworks</b-nav-item>
-						</b-navbar-nav>
-
-					</b-collapse>
 
 				</b-container>
 			</b-navbar>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -3,10 +3,10 @@
     <b-container>
       <b-row>
         <b-col>
-          <router-link to="accidents" class="btn btn-primary btn-lg btn-block">Accidents</router-link>
+          <router-link to="accidents" class="btn btn-primary btn-lg btn-block py-5"><b-img center class="w-50 py-3" :src="require('@/assets/home-white/car-collision.svg')" fluid/><strong class="mb-5">Accidents</strong></router-link>
         </b-col>
         <b-col>
-          <router-link to="roadworks" class="btn btn-primary btn-lg btn-block">Roadworks</router-link>
+          <router-link to="roadworks" class="btn btn-primary btn-lg btn-block py-5"><b-img center class="w-50 py-3" :src="require('@/assets/home-white/worker-with-shovel.svg')" fluid style="height: 190px"/><strong class="mb-5">Roadworks</strong></router-link>
         </b-col>
       </b-row>
 
