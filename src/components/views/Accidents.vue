920b946440236a7ce4b4ab7ba23600ddcba4bbef
Ollie Reardon <ojreardon@gmail.com>
2019-01-11 13:56:28 +0000

    Height adjust for home view

--- src/components/views/Accidents.vue
+++ src/components/views/Accidents.vue
@@ -1,6 +1,8 @@
 <template>
+  <pull-to :top-load-method="refresh">
   <section class="accidents">
     <b-container>
+
       <div v-for="report in reports" :key="report.guid._Data" class="text-capitalize">
         <hr>
         <h2>{{report.title}}</h2>
@@ -9,20 +11,38 @@
       </div>
     </b-container>
   </section>
+</pull-to>
 </template>
 <script>
 import axios from 'axios';
 import XML from 'pixl-xml';
-
+import PullTo from 'vue-pull-to';
+const BOTTOM_DEFAULT_CONFIG = {
+  pullText: 'Refresh...', // The text is displayed when you pull down
+  triggerText: 'Loading...', // The text that appears when the trigger distance is pulled down
+  loadingText: 'Loading...', // The text in the load
+  doneText: 'Done!', // Load the finished text
+  failText: 'Failed to refresh.', // Load failed text
+  loadedStayTime: 400, // Time to stay after loading ms
+  stayDistance: 50, // Trigger the distance after the refresh
+  triggerDistance: 70 // Pull down the trigger to trigger the distance
+}
 export default {
+
   name: 'accidents',
+  components: {
+      PullTo
+    },
   data() {
     return {
       reports: [],
       errors: []
     }
   },
-mounted() {
+
+methods: {
+  refresh(loaded) {
+    console.log('hello');
     axios.get('https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents.xml')
     .then(response => {
       this.reports = XML.parse(response.data);
@@ -30,8 +50,13 @@ mounted() {
     })
     .catch(e => {
       this.errors.push(e)
-    })
+    });
+    loaded('done')
+  }
+},
 
+mounted() {
+    this.refresh();
   }
 }
 </script>
